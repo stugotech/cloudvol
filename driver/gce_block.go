7c1eb98d4933c53871d1675eff5441ec5b948b7c
Gordon Mackenzie-Leigh <gordon@stugo.co.uk>
2017-02-18 11:21:24 +0000

    improve gce logging

--- driver/gce_block.go
+++ driver/gce_block.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"time"
 
+	"os"
+
 	"cloud.google.com/go/compute/metadata"
 	log "github.com/Sirupsen/logrus"
 	"golang.org/x/net/context"
@@ -28,47 +30,50 @@ type gceDriver struct {
 // NewGceDriver creates a new instance of the GCE volume driver
 func NewGceDriver() (BlockDriver, error) {
 	if !metadata.OnGCE() {
-		log.Warn("not running on GCE")
-		return nil, fmt.Errorf("not running on GCE")
+		log.Warn("GCE: not on GCE or can't contact metadata server")
+		return nil, fmt.Errorf("GCE: not on GCE or can't contact metadata server")
+	}
+
+	creds := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+
+	if creds != "" {
+		log.WithFields(log.Fields{"file": creds}).Info("GCE: using credentials from GOOGLE_APPLICATION_CREDENTIALS")
+	} else {
+		log.Info("GCE: using instance default credentials")
 	}
 
 	ctx := context.Background()
 
 	client, err := google.DefaultClient(ctx, compute.ComputeScope)
 	if err != nil {
-		log.WithError(err).Warn("error creating GCE client")
-		return nil, fmt.Errorf("error creating GCE client: %s", err)
+		return nil, fmt.Errorf("GCE: error creating client: %s", err)
 	}
 
 	computeService, err := compute.New(client)
 	if err != nil {
-		log.WithError(err).Warn("error creating GCE client")
-		return nil, fmt.Errorf("error creating GCE client: %s", err)
+		return nil, fmt.Errorf("GCE: error creating client: %s", err)
 	}
 
 	instance, err := metadata.InstanceName()
 	if err != nil {
-		log.WithError(err).Warn("error retrieving GCE instance name")
-		return nil, fmt.Errorf("error retrieving instance name: %s", err)
+		return nil, fmt.Errorf("GCE: error retrieving instance name: %s", err)
 	}
 
 	zone, err := metadata.Zone()
 	if err != nil {
-		log.WithError(err).Warn("error retrieving GCE zone")
-		return nil, fmt.Errorf("error retrieving zone: %s", err)
+		return nil, fmt.Errorf("GCE: error retrieving zone: %s", err)
 	}
 
 	project, err := metadata.ProjectID()
 	if err != nil {
-		log.WithError(err).Warn("error retrieving GCE project ID")
-		return nil, fmt.Errorf("error retrieving project ID: %s", err)
+		return nil, fmt.Errorf("GCE: error retrieving project ID: %s", err)
 	}
 
 	log.WithFields(log.Fields{
 		"instance": instance,
 		"zone":     zone,
 		"project":  project,
-	}).Info("detected instance parameters")
+	}).Info("GCE: detected instance parameters")
 
 	provider := &gceDriver{
 		client:   computeService,
@@ -81,7 +86,7 @@ func NewGceDriver() (BlockDriver, error) {
 
 // Attach attaches a disk to the current instance
 func (d *gceDriver) Attach(name string) (string, error) {
-	log.WithFields(log.Fields{"name": name}).Info("attach disk")
+	log.WithFields(log.Fields{"name": name}).Info("GCE: attach disk")
 	device := fmt.Sprintf(deviceBaseName, name)
 
 	disk := compute.AttachedDisk{
@@ -91,7 +96,6 @@ func (d *gceDriver) Attach(name string) (string, error) {
 	op, err := d.client.Instances.AttachDisk(d.project, d.zone, d.instance, &disk).Do()
 
 	if err != nil {
-		log.WithError(err).Warn("error attaching disk")
 		return "", fmt.Errorf("error attaching disk: %s", err)
 	}
 
@@ -102,21 +106,21 @@ func (d *gceDriver) Attach(name string) (string, error) {
 	}
 	// return the path to the device
 	devicePath := fmt.Sprintf(devicePathFormat, device)
-	log.WithFields(log.Fields{"devicePath": devicePath}).Info("attached device")
+	log.WithFields(log.Fields{"devicePath": devicePath}).Info("GCE: attached device")
 	return devicePath, nil
 }
 
 // Detach detaches a disk from the current instance
 func (d *gceDriver) Detach(name string) error {
-	log.WithFields(log.Fields{"name": name}).Info("detach disk")
+	log.WithFields(log.Fields{"name": name}).Info("GCE: detach disk")
 	device := fmt.Sprintf(deviceBaseName, name)
 
 	op, err := d.client.Instances.DetachDisk(d.project, d.zone, d.instance, device).Do()
 	if err != nil {
-		log.WithError(err).Warn("error detaching disk")
 		return err
 	}
 
+	log.WithFields(log.Fields{"name": name}).Info("GCE: detach disk success")
 	return d.waitForOp(op)
 }
 
@@ -128,7 +132,7 @@ func (d *gceDriver) waitForOp(op *compute.Operation) error {
 			"project":   d.project,
 			"zone":      d.zone,
 			"operation": op.Name,
-		}).Info("waiting for GCE operation")
+		}).Info("GCE: wait for operation")
 
 		if op, err := d.client.ZoneOperations.Get(d.project, d.zone, op.Name).Do(); err == nil {
 			log.WithFields(log.Fields{
@@ -136,7 +140,7 @@ func (d *gceDriver) waitForOp(op *compute.Operation) error {
 				"zone":      d.zone,
 				"operation": op.Name,
 				"status":    op.Status,
-			}).Info("GCE operation status")
+			}).Info("GCE: operation status")
 
 			if op.Status == "DONE" {
 				return nil
@@ -147,7 +151,7 @@ func (d *gceDriver) waitForOp(op *compute.Operation) error {
 				"operation":  op.Name,
 				"targetLink": op.TargetLink,
 				"error":      err,
-			}).Warn("error while getting GCE operation")
+			}).Warn("GCE: error while getting operation")
 		}
 	}
 
@@ -155,7 +159,7 @@ func (d *gceDriver) waitForOp(op *compute.Operation) error {
 		"operation":  op.Name,
 		"targetLink": op.TargetLink,
 		"timeout":    operationWaitTimeout,
-	}).Warn("timeout while waiting for GCE operation to complete")
+	}).Warn("GCE: timeout while waiting for operation to complete")
 
-	return fmt.Errorf("timeout while waiting for operation %s on %s to complete", op.Name, op.TargetLink)
+	return fmt.Errorf("GCE: timeout while waiting for operation %s on %s to complete", op.Name, op.TargetLink)
 }
